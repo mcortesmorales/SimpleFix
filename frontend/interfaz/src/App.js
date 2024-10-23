9ab0a52251f69d08dfb3fa44ad5095ed45d37052
mcortesm@alumnosuls.cl <mcortesmorales98@gmail.com>
2024-10-22 23:26:36 -0300

    rutas frontend

--- frontend/interfaz/src/App.js
+++ frontend/interfaz/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/login';
+import LoginPage from './pages/LoginPage';
 import AdminPanel from './pages/adminPanel';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <AdminPanel></AdminPanel>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/interfaz/src/pages/LoginPage.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/users', { username, password });
+      console.log(response.data.message);
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4 shadow px-5">
+        <h2 className="text-center mb-4">Iniciar Sesión</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">Nombre de usuario</label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Contraseña</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">Iniciar Sesión</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
