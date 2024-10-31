0ed3ec41a832e67716c45fcc62c4accb3240cc68
BenjaSilva19 <benjamin.silva@userena.cl>
2024-10-31 19:42:50 -0300

    Se añadio la pagina que permite subir el archivo csv para su modificacion

--- frontend/interfaz/src/App.js
+++ frontend/interfaz/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import AdminPanel from './pages/adminPanel';
 import Header from './components/Header';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import PrivateRoute from './components/PrivateRoute';
+import DropPage from './pages/dropPage';
 
 function App() {
   return (
@@ -15,13 +16,15 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route
-            path="/admin"
-            element={
+            path="/admin" element={
               <PrivateRoute>
                 <AdminPanel />
               </PrivateRoute>
             }
           />
+          <Route path="/admin2" element={<AdminPanel />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/file-drop" element={<DropPage />} />
         </Routes>
         </AuthProvider>
       </Router>
